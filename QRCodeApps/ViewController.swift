81c44e92b15e8bd557d5d45e90a4105199927a13
Auzan Lazuardi <auzan.lazuardi@mola.tv>
2024-01-30 19:35:04 +0700

    Initial Commit

---
+++ QRCodeApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeApps
+//
+//  Created by Auzan Lazuardi on 30/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
